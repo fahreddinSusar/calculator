13290622618694dc762b9c18c54ff1d7f6dd7e58
Fahreddin Susar <fahreddin17@gmail.com>
2022-01-24 19:55:53 +0300

    Rearrange the multiplication funciton to work with number inputs

--- js-file.js
+++ js-file.js
@@ -5,9 +5,9 @@ function add(num1, num2) {
 function subtract(numberOne, numberTwo) {
     return numberOne - numberTwo
 };
-function multiply(arr) {
-    if (arr.length < 1) { return 0 }
-    return arr.reduce((cumulative, currentValue) => cumulative * currentValue, 1);
+function multiply(num1, num2) {
+    return num1 * num2
+    
 };
 
 function power(base, power) {
