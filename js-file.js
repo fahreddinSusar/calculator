4fe2abb9cbc930795be8eab517ac8c9b19d45c27
Fahreddin Susar <fahreddin17@gmail.com>
2022-01-26 22:08:40 +0300

    Create funcionality of the calculator

--- js-file.js
+++ js-file.js
@@ -1,3 +1,56 @@
+let displayValue = "";
+let operator = "";
+const inputButtons = document.querySelectorAll('.digit-buttons, .operator-buttons');
+const displayScreen = document.querySelector('.display');
+const equalButton = document.querySelector('#button-equal');
+const clearButton = document.querySelector('#button-clear');
+
+displayScreen.textContent = displayValue;
+inputButtons.forEach(button =>button.addEventListener('click',populateDisplay));
+equalButton.addEventListener('click', computeResult);
+clearButton.addEventListener('click', clear);
+function clear(e) {
+    displayValue = '';
+    operator = '';
+    displayScreen.innerText = '';
+}
+
+function computeResult(e) {
+    const firstNumber = Number(displayValue.split(operator)[0]);
+    const secondNumber = Number(displayValue.split(operator)[1]);
+    switch (operator) {
+        case '+':
+            displayScreen.innerText = add(firstNumber, secondNumber);
+            operator = '';
+            displayValue = '';
+            break;
+        case '-':
+            displayScreen.innerText = subtract(firstNumber, secondNumber);
+            operator = '';
+            displayValue = '';
+            break;
+        case 'x':
+            displayScreen.innerText = multiply(firstNumber, secondNumber);
+            operator = '';
+            displayValue = '';
+            break;
+        case '/':
+            displayScreen.innerText = divide(firstNumber, secondNumber);
+            operator = '';
+            displayValue = '';
+            break;
+        default:
+            break;
+    }
+}
+function populateDisplay(e) {
+    displayValue += e.target.innerText;
+    displayScreen.textContent = displayValue;
+    if(e.target.innerText === '+' || e.target.innerText === '-' || e.target.innerText === 'x' || e.target.innerText === '/') {
+        operator = e.target.innerText;
+    }
+}
+
 function add(num1, num2) {
     return num1 + num2
 };
